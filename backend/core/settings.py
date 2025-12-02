fc1fbc29f97bb65eea80752279d8444f1155fe12
dibi-nzogue <nzogue.dibiye@gmail.com>
2025-12-02 20:16:16 +0100

    version 4

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -319,7 +319,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1,christlandtech.onrender.com"
+).split(",")
+
 
 DATABASES = {
     'default': {
