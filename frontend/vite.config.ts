caa19ef1f24176103517fa8fba600a90e7bbbe7e
dibi-nzogue <nzogue.dibiye@gmail.com>
2025-12-07 06:12:35 +0100

    version 4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- // ✅ IMPORTANT pour que /a-propos, /produits, etc. chargent bien les assets
+  base: "/", // ✅ IMPORTANT pour que /a-propos, /produits, etc. chargent bien les assets
   plugins: [
     react({
       babel: {
