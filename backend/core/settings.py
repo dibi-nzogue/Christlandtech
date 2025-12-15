a11f16966b8dcbb4442fb5d6b9afb94465cf75cd
dibi-nzogue <nzogue.dibiye@gmail.com>
2025-12-15 19:58:47 +0100

    version 5

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -496,7 +496,24 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django.request": {  # log 500 + traceback
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
 
 # === DRF ===
 REST_FRAMEWORK = {
